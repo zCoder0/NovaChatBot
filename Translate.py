b3a8df1aacaba98b546ac75c619efa6dceb27a57
zCoder0 <131846622+zCoder0@users.noreply.github.com>
2024-11-15 17:22:53 +0530

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -1,5 +1,5 @@
 from googletrans import Translator
-import language_tool_python 
+
 from LangModel import LangsModel
 class Translation:
     def __init__(self):
@@ -10,20 +10,10 @@ class Translation:
         lng = l.predict(text)
         return lng
     
-    def correct_grammar(self,text):
-        # Initialize the LanguageTool model for English
-        tool = language_tool_python.LanguageTool('en-US')
-        
-        # Correct grammar and spelling mistakes
-        matches = tool.check(text)
-        corrected_text = language_tool_python.utils.correct(text, matches)
-        return corrected_text
 
     def translate (self, text , ln='en'):
         text = str(text)
         lng =self.detectLang(text)
-        if lng == "English":
-            text = self.correct_grammar(text)
         translated = self.translator.translate(text, src=str(lng), dest=ln)  # Translate from English to Spanish
         return str(translated.text) , lng
 
